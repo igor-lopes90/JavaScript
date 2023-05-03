4ba56d97acfe2d04fa4f6c138ef15b681f5775e6
Igor Lopes <igor_lopes90@yahoo.com.br>
2023-05-03 00:24:53 -0300

    aula 13 + 1 exercicio

---
+++ aula13/ambiente.js
@@ -0,0 +1,30 @@
+/*
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
+
+/* WHILE
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+/* DO WHILE
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 3) 
+*/
+
+console.log('Vai Começar...')
+for (var c = 1; c <= 10; c++) {
+    console.log(c)
+}
+console.log('FIM!!!)')
