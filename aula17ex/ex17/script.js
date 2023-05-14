79686f0261db088b18ea4404b5a54375641a8b4f
Igor Lopes <igor_lopes90@yahoo.com.br>
2023-05-13 23:57:03 -0300

    criação aula17

---
+++ aula17ex/ex17/script.js
@@ -0,0 +1,14 @@
+function adicionar() {
+    var num = document.getElementById('numtxt')
+    
+    if (num.value.length == '') {
+        window.alert('Por favor, digite um número válido!')
+    } else if (num.value > 100 || num.value < 1){
+        window.alert('Por favor, digite um número entre 1 e 100!')
+    } else {
+        var n = Number(num.value)
+        var c = 1
+        while
+    }
+
+}
