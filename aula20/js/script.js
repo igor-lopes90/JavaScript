25bf230f120bfbd8d021a2a10aaf1547cc517374
Igor Lopes <igor_lopes90@yahoo.com.br>
2023-07-03 08:50:03 -0300

    estudo

---
+++ aula20/js/script.js
@@ -0,0 +1,10 @@
+function meuEscopo () {
+    const form = document.querySelector('.form');
+
+    form.onsubmit = function (evento) {
+        evento.preventDefault();
+        alert(1);
+        console.log('Foi enviado.');
+    };
+}
+meuEscopo();
